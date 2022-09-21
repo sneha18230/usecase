21af4cbfd1b73acad7e8a25efa6aae14a317c6ec
sneha18230 <114058243+sneha18230@users.noreply.github.com>
2022-09-21 13:35:51 +0530

    Add files via upload

---
+++ eAuction.BuyerApi/Controllers/BuyerController.cs
@@ -0,0 +1,123 @@
+﻿using Auction.Common.Exception;
+using Auction.Common.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace eAuction.BuyerApi.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [Produces("application/json")]
+    [ApiController]
+    [ExcludeFromCodeCoverage]
+    public class BuyerController : ControllerBase
+    {
+        private readonly ILogger<BuyerController> _logger;
+
+        private readonly IBuyerService _service;
+
+        public BuyerController(ILogger<BuyerController> logger, IBuyerService service)
+        {
+            this._logger = logger;
+
+            this._service = service;
+        }
+
+        [HttpPost("place-bid")]
+        [Produces(typeof(Auction.Commom.Model.Buyer))]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(500)]
+        public virtual async Task<ActionResult<Auction.Commom.Model.Buyer>> AddBid(Auction.Commom.Model.Buyer bid)
+        {
+            _logger.LogInformation("Method Started : " + MethodBase.GetCurrentMethod().Name);
+
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
+
+                var response = await _service.AddBid(bid).ConfigureAwait(false);
+
+                return Ok(response);
+            }
+
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+
+                if (ex is ProductException)
+                {
+                    return NotFound(ex.Message);
+                }
+
+                else if (ex is BuyerException || ex is ValidationException)
+                {
+                    return BadRequest(ex.Message);
+                }
+
+                else
+                {
+                    return new StatusCodeResult(StatusCodes.Status500InternalServerError);
+                }
+            }
+
+            finally
+            {
+                _logger.LogInformation("Method Ended : " + MethodBase.GetCurrentMethod().Name);
+            }
+        }
+
+        [HttpPut("update-bid/{productId}/{buyerEmailld}/{newBidAmount}")]
+        [Produces(typeof(Auction.Commom.Model.Buyer))]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(500)]
+        public virtual async Task<ActionResult<Auction.Commom.Model.Buyer>> UpdateBid(int productId, string buyerEmailld, double newBidAmount)
+        {
+            _logger.LogInformation("Method Started : " + MethodBase.GetCurrentMethod().Name);
+
+            try
+            {
+                var response = await _service.UpdateBid(productId, buyerEmailld, newBidAmount).ConfigureAwait(false);
+
+                return Ok(response);
+            }
+
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+
+                if (ex is ProductException || ex is BuyerException)
+                {
+                    return NotFound(ex.Message);
+                }
+
+                else if (ex is ValidationException)
+                {
+                    return BadRequest(ex.Message);
+                }
+
+                else
+                {
+                    return new StatusCodeResult(StatusCodes.Status500InternalServerError);
+                }
+            }
+
+            finally
+            {
+                _logger.LogInformation("Method Ended : " + MethodBase.GetCurrentMethod().Name);
+            }
+        }
+
+
+    }
+}
---
+++ eAuction.BuyerApi/Startup.cs
@@ -0,0 +1,86 @@
+using Auction.Commom.Context;
+using Auction.Commom.Repository;
+using Auction.Common.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using System.Diagnostics.CodeAnalysis;
+using System.Text.Json.Serialization;
+
+namespace Buyer
+{
+    [ExcludeFromCodeCoverage]
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddLogging();
+
+            services.AddScoped<IAuctionMongoContext, AuctionMongoContext>();
+
+            services.AddScoped<IProductRepository, ProductRepository>();
+
+            services.AddScoped<ISellerService, SellerService>();
+
+            services.AddScoped<IBuyerService, BuyerService>();
+
+            services.AddCors();
+
+            services.AddControllers()
+                .AddJsonOptions(opts =>
+                {
+                    opts.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+
+                    opts.JsonSerializerOptions.IgnoreNullValues = true;
+                });
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "eAuction.BuyerApi", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "eAuction.BuyerApi v1"));
+
+            app.UseCors(builder =>
+            {
+                builder
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader();
+            });
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
